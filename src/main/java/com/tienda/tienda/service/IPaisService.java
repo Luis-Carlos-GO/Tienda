1057f39dc3d2c0ed5870de115b030ec6b113c45f
LuisCarlosGonzalez <carlosgears2012@hotmail.com>
2022-10-17 11:15:56 -0600

    Commit semana 4-5

---
+++ src/main/java/com/tienda/tienda/service/IPaisService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.tienda.service;
+
+/**
+ *
+ * @author Lichkhing
+ */
+import com.tienda.tienda.entity.Pais;
+import java.util.List;
+
+public interface IPaisService {
+    public List<Pais> listCountry();
+}
